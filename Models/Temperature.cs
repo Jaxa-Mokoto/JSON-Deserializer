a442376f672875108420e1f8fa381120b49abf93
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 14:27:12 +0800

    added functionality to deserialise json document without class definition

--- Models/Temperature.cs
+++ Models/Temperature.cs
@@ -2,14 +2,15 @@
 // using Visual Studio's option Edit > Paste Special > Paste JSON as classes
 namespace Deserializer.Models
 {
-    public partial class Temperature
-    {
-        public DateTimeOffset Date { get; set; }
+    // Uncomment if using deserialisation option 1 or 2 in Program.cs
+    //public partial class Temperature
+    //{
+    //    public DateTimeOffset Date { get; set; }
 
-        public long TemperatureC { get; set; }
+    //    public long TemperatureC { get; set; }
 
-        public string? Summary { get; set; }
+    //    public string? Summary { get; set; }
 
-        public long TemperatureF { get; set; }
-    }
+    //    public long TemperatureF { get; set; }
+    //}
 }
--- Program.cs
+++ Program.cs
@@ -3,27 +3,28 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using Deserializer.Models;
 
-var opt = new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = true //ignores case sensitivity
-};
+// uncomment to use for option 1 and option 2
+//var opt = new JsonSerializerOptions
+//{
+//    PropertyNameCaseInsensitive = true //ignores case sensitivity
+//};
 
 HttpClient client = new()
 {
     BaseAddress = new Uri("http://localhost:5043")
 };
 
-// helper to deserialise JSON response
-var temperatures = await client.GetFromJsonAsync<Temperature[]>("/weatherforecast", opt);
-if (temperatures != null)
-{
-    foreach (var temperature in temperatures)
-    {
-        Console.WriteLine($"Summary: {temperature.Summary}");
-    }
-}
+// 1. The code below will use a helper to deserialise JSON response (option 1)
+//var temperatures = await client.GetFromJsonAsync<Temperature[]>("/weatherforecast", opt);
+//if (temperatures != null)
+//{
+//    foreach (var temperature in temperatures)
+//    {
+//        Console.WriteLine($"Summary: {temperature.Summary}");
+//    }
+//}
 
-// The code below will also deserialize an API response without a helper
+// 2. The code below will also deserialize an API response without a helper (option 2)
 //var response = await client.GetAsync("/weatherforecast");
 
 //if (response.IsSuccessStatusCode)
@@ -43,3 +44,26 @@ if (temperatures != null)
 //{
 //    Console.WriteLine($" Whoops, something went wrong: {response.StatusCode}");
 //}
+
+// 3. JSON Document Deserialisation (option 3) 
+var response = await client.GetAsync("/weatherforecast");
+
+if (response.IsSuccessStatusCode)
+{
+    // puts unserialised json data into the jsonString variable
+    var jsonString = await response.Content.ReadAsStringAsync();
+
+    using (JsonDocument jsonDocument = JsonDocument.Parse(jsonString))
+    {
+        JsonElement root = jsonDocument.RootElement;
+
+        foreach (var temperature in root.EnumerateArray())
+        {
+            Console.WriteLine(temperature.GetProperty("summary").ToString()); // should not use magic string like this.
+        }
+    }
+}
+else
+{
+    Console.WriteLine($" Whoops, something went wrong: {response.StatusCode}");
+}
