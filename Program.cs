c191f1eb0953fb60263de4775cf5e95a9b026a92
Jaxa Mokoto <modjadji.a.mokoto@gmail.com>
2023-02-08 14:13:30 +0800

    added deserialising json with a helper method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json;
 using Deserializer.Models;
 
@@ -12,22 +13,33 @@ HttpClient client = new()
     BaseAddress = new Uri("http://localhost:5043")
 };
 
-var response = await client.GetAsync("/weatherforecast");
-
-if (response.IsSuccessStatusCode)
+// helper to deserialise JSON response
+var temperatures = await client.GetFromJsonAsync<Temperature[]>("/weatherforecast", opt);
+if (temperatures != null)
 {
-    // create a collection of temperatures
-    var temperatures = await JsonSerializer.DeserializeAsync<Temperature[]>(await response.Content.ReadAsStreamAsync(), opt);
-    if (temperatures != null)
+    foreach (var temperature in temperatures)
     {
-        foreach(var temperature in temperatures)
-        {
-            Console.WriteLine($"Summary: {temperature.Summary}");
-        }
+        Console.WriteLine($"Summary: {temperature.Summary}");
     }
+}
 
-}
-else
-{
-    Console.WriteLine($" Whoops, something went wrong: {response.StatusCode}");
-}
+// The code below will also deserialize an API response without a helper
+//var response = await client.GetAsync("/weatherforecast");
+
+//if (response.IsSuccessStatusCode)
+//{
+//    // create a collection of temperatures
+//    var temperatures = await JsonSerializer.DeserializeAsync<Temperature[]>(await response.Content.ReadAsStreamAsync(), opt);
+//    if (temperatures != null)
+//    {
+//        foreach(var temperature in temperatures)
+//        {
+//            Console.WriteLine($"Summary: {temperature.Summary}");
+//        }
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine($" Whoops, something went wrong: {response.StatusCode}");
+//}
